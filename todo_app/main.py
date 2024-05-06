60c7a06d22bf556686d875925bb4356ba186934c
dev-shayan <devvshayan@gmail.com>
2024-05-06 14:55:53 +0500

    backend completed without testing

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import SQLModel, Field, create_engine, Session, select
 from todo_app import settings
 from typing import Annotated
 from contextlib import asynccontextmanager
-
-# create model
-# data model
-# table model
+# Step-1: Create Database on Neon
+# Step-2: Create .env file for environment variables
+# Step-3: Create setting.py file for encrypting DatabaseURL
+# Step-4: Create a Model
+# Step-5: Create Engine
+# Step-6: Create function for table creation
+# Step-7: Create function for session management
+# Step-8: Create contex manager for app lifespan
+# Step-9: Create all endpoints of todo app
 
 
 class Todo(SQLModel, table=True):
@@ -37,20 +42,6 @@ def create_tables():
     SQLModel.metadata.create_all(engine)
 
 
-# todo1: Todo = Todo(content="first task")
-# todo2: Todo = Todo(content="Second task")
-
-# # session : Seperate session for each functionality/transaction
-# session = Session(engine)
-
-# # create todos in db
-# session.add(todo1)
-# session.add(todo2)
-# print(f"Before Commit todo1={todo1}")
-# session.commit()
-# session.refresh(todo1)
-# print(f"After Commit todo1={todo1}")
-# session.close()
 
 #this is a generator function we uses with to cuz it automaticallty close the session when the code inside this function is executed and the yield will wait for the remaining code to be executed 
 def get_session():
@@ -82,23 +73,48 @@ async def create_todo(todo:Todo,session:Annotated[Session,Depends(get_session)])
     return todo
 
 #read krne k liye get method of http
-@app.get("/todos/", response_model=list[Todo])
-async def get_all(session:Annotated[Session,Depends(get_session)]): 
-    statement = select(Todo)
-    todos = session.exec(statement).all()
-    return todos
+@app.get('/todos/', response_model=list[Todo])
+async def get_all(session: Annotated[Session, Depends(get_session)]):
+    todos = session.exec(select(Todo)).all()
+    if todos:
+        return todos
+    else:
+        raise HTTPException(status_code=404, detail="No Task found")
+
 
 @app.get("/todos/{id}",response_model=Todo)
 async def get_single_todo(id:int, session:Annotated[Session,Depends(get_session)]): 
-    todo = session.exec(select(Todo).where(Todo.id==id)).first()
-    return todo
+    todo = session.exec(select(Todo).where(Todo.id == id)).first()
+    if todo:
+        return todo
+    else:
+        raise HTTPException(status_code=404, detail="No Task found")
 
 #Update/edit krne k liye put/patch method of http
 #put delete the existing entery and create new one and need complete data to do its function
 #patch only need requierd data to do its functionality
-@app.put("/todos/{id}")
-async def edit_todo(): ...
+@app.put('/todos/{id}')
+async def edit_todo(id: int, todo: Todo, session: Annotated[Session, Depends(get_session)]):
+    existing_todo = session.exec(select(Todo).where(Todo.id == id)).first()
+    if existing_todo:
+        existing_todo.content = todo.content
+        existing_todo.is_completed = todo.is_completed
+        session.add(existing_todo)
+        session.commit()
+        session.refresh(existing_todo)
+        return existing_todo
+    else:
+        raise HTTPException(status_code=404, detail="No task found")
 
 #delete krne k liye delete method of http
-@app.delete("/todos/{id}")
-async def delete_todo(): ...
+@app.delete('/todos/{id}')
+async def delete_todo(id: int, session: Annotated[Session, Depends(get_session)]):
+    todo = session.exec(select(Todo).where(Todo.id == id)).first()
+    # todo = session.get(Todo,id)
+    if todo:
+        session.delete(todo)
+        session.commit()
+        # session.refresh(todo)
+        return {"message": "Task successfully deleted"}
+    else:
+        raise HTTPException(status_code=404, detail="No task found")
