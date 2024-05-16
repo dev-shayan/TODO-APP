2754ebeebfb32645b22b36abd3194369a2cbedd1
dev-shayan <devvshayan@gmail.com>
2024-05-16 13:02:53 +0500

    refactored code with fixture following DRY principle

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
 from fastapi.testclient import TestClient
-from sqlmodel import SQLModel, create_engine, Session, select
-from fastapi import FastAPI
+from sqlmodel import SQLModel, create_engine, Session
 from todo_app import settings
 from todo_app.main import app, get_session
 import pytest
@@ -15,112 +14,143 @@ engine = create_engine(
     pool_size=10,
     echo=True,
 )
+#####################################################
+#refacter with pytest fixture
+
+# 4 steps of testing
+# 1 - Arrange 2-Act 3-Assert 4-CleanUp
+# Arrange - set up the test like the prerequisites or resources that are needed for the test
+    #SQLModel.metadata.create_all(engine)
+    # with Session(engine) as session:
+    #     def db_session_override():
+    #         return session
+    # app.dependency_overrides[get_session] = db_session_override
+    # test_app = TestClient(app=app)
+# Act - perform the action that you want to test
+# Assert - check the result of the action
+# CleanUp - clean up the resources that you have used in the test
+#Fixtures automate the Arrange and CleanUp steps of testing
+############################################################
+
+@pytest.fixture(scope="module",autouse = True)
+def get_db_session():
+    SQLModel.metadata.create_all(engine)
+    yield Session(engine)
+
+@pytest.fixture(scope="function")
+def test_app(get_db_session):
+    def test_session():
+        yield get_db_session
+    app.dependency_overrides[get_session] = test_session
+    with TestClient(app=app) as test_app:
+        yield test_app
+
+#######################################################
 
 
 # Root Test
 def test_root():
-    client = TestClient(app=app)
-    response = client.get("/")
+    test_app = TestClient(app=app)
+    response = test_app.get("/")
     data = response.json()
     assert response.status_code == 200
     assert data == {"msg": "helo g"}
 
 
 # Post test
-def test_create_todo():
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
+def test_create_todo(test_app):
+    # SQLModel.metadata.create_all(engine)
+    # with Session(engine) as session:
 
-        def db_session_override():
-            return session
+    #     def db_session_override():
+    #         return session
 
-    app.dependency_overrides[get_session] = db_session_override
-    client = TestClient(app=app)
+    # app.dependency_overrides[get_session] = db_session_override
+    # client = TestClient(app=app)
     test_todo = {"content": "create todo test", "is_completed": False}
-    response = client.post("/todos/", json=test_todo)
+    response = test_app.post("/todos/", json=test_todo)
     data = response.json()
     assert response.status_code == 200
     assert data["content"] == test_todo["content"]
 
 
 # get_all test
-def test_get_all():
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
+def test_get_all(test_app):
+    # SQLModel.metadata.create_all(engine)
+    # with Session(engine) as session:
 
-        def db_session_override():
-            return session
+    #     def db_session_override():
+    #         return session
 
-    app.dependency_overrides[get_session] = db_session_override
-    client = TestClient(app=app)
+    # app.dependency_overrides[get_session] = db_session_override
+    # test_app = TestClient(app=app)
     test_todo = {"content": "get all todo test", "is_completed": False}
-    response = client.post("/todos/", json=test_todo)
-    data = response.json()
+    response = test_app.post("/todos/", json=test_todo)
 
-    response = client.get("/todos")
+    response = test_app.get("/todos")
     new_todo = response.json()[-1]
     assert response.status_code == 200
     assert new_todo["content"] == test_todo["content"]
 
 
 # single todo test
-def test_get_single_todo():
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
+def test_get_single_todo(test_app):
+    # SQLModel.metadata.create_all(engine)
+    # with Session(engine) as session:
 
-        def db_session_override():
-            return session
+    #     def db_session_override():
+    #         return session
 
-    app.dependency_overrides[get_session] = db_session_override
-    client = TestClient(app=app)
+    # app.dependency_overrides[get_session] = db_session_override
+    # test_app = TestClient(app=app)
 
     test_todo = {"content": "get single todo test", "is_completed": False}
-    response = client.post("/todos/", json=test_todo)
+    response = test_app.post("/todos/", json=test_todo)
     todo_id = response.json()["id"]
-    res = client.get(f"todos/{todo_id}")
+    res = test_app.get(f"todos/{todo_id}")
     data = res.json()
     assert res.status_code == 200
     assert data["content"] == test_todo["content"]
 
 
 # edit todo test
-def test_edit_todo():
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
+def test_edit_todo(test_app):
+    # SQLModel.metadata.create_all(engine)
+    # with Session(engine) as session:
 
-        def db_session_override():
-            return session
+    #     def db_session_override():
+    #         return session
 
-    app.dependency_overrides[get_session] = db_session_override
-    client = TestClient(app=app)
+    # app.dependency_overrides[get_session] = db_session_override
+    # test_app = TestClient(app=app)
 
     test_todo = {"content": "edit todo test", "is_completed": False}
-    response = client.post("/todos/", json=test_todo)
+    response = test_app.post("/todos/", json=test_todo)
     todo_id = response.json()["id"]
 
     edited_todo = {"content": "we have edited this", "is_completed": False}
-    response = client.put(f"/todos/{todo_id}", json=edited_todo)
+    response = test_app.put(f"/todos/{todo_id}", json=edited_todo)
     data = response.json()
     assert response.status_code == 200
     assert data["content"] == edited_todo["content"]
 
 
 # delete todo
-def test_delete_todo():
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
+def test_delete_todo(test_app):
+    # SQLModel.metadata.create_all(engine)
+    # with Session(engine) as session:
 
-        def db_session_override():
-            return session
+    #     def db_session_override():
+    #         return session
 
-    app.dependency_overrides[get_session] = db_session_override
-    client = TestClient(app=app)
+    # app.dependency_overrides[get_session] = db_session_override
+    # test_app = TestClient(app=app)
 
     test_todo = {"content": "delete todo test", "is_completed": False}
-    response = client.post("/todos/", json=test_todo)
+    response = test_app.post("/todos/", json=test_todo)
     todo_id = response.json()["id"]
 
-    response = client.delete(f"/todos/{todo_id}")
+    response = test_app.delete(f"/todos/{todo_id}")
     data = response.json()
     assert response.status_code == 200
     assert data["message"] == "Task successfully deleted"
