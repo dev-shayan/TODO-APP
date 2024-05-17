9c1981aca1fed220cd908abe886477056886aca7
dev-shayan <devvshayan@gmail.com>
2024-05-17 14:26:54 +0500

    removed unwanted lines of code

--- todo-app-frontend/src/components/component/table.tsx
+++ todo-app-frontend/src/components/component/table.tsx
@@ -1,24 +1,8 @@
 import React from "react";
-import Single_todo from "./single_todo";
-import { Todo } from "../../../todo";
 import  Todocomponent  from "./todocomponent";
 
 export default function Table() {
   return (
-    // <table className="w-full ">
-    //   <thead>
-    //     <tr className=" rounded-md drop-shadow-xl">
-    //       {/* <th>ID</th> */}
-    //       <th>Tasks</th>
-    //       <th>Actions</th>
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     {task.map((task: Todo) => (
-    //       <Single_todo key={task.id} task={task} />
-    //     ))}
-    //   </tbody>
-    // </table>
     <Todocomponent />
   );
 }
